54c5c6dfd8762d9d8d4e0fcb5da6357b6098c781
Ryumon X <dewangga.mustika12@gmail.com>
2025-07-18 17:10:56 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,24 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.setGlobalPrefix('api');
   app.use(cookieParser());
 
+  const origin = configService.get<string>('CORS_ORIGIN');
+  const allowedOrigins = origin?.split(',').map((o) => o.trim());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000); 
+  await app.listen(4000);
 }
 bootstrap();
